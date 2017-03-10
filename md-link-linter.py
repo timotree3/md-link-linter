794b3c65eb18a26b8d1702b93f3e50f38a6bd2ee
timotree3 <timorcb@gmail.com>
2017-03-09 23:06:30 -0500

    Rewrote codebase and added most functionality
    :tada:

--- md-link-linter.py
+++ md-link-linter.py
@@ -1,24 +1,69 @@
-#!/bin/env python3
-import os
-import tempfile
-import shutil
-import argparse
-parser = argparse.ArgumentParser('md-link-linter')
-parser.add_argument('repo', help="the git repository to lint")
+#!/usr/bin/env python3
+from subprocess import run, PIPE
+from os import chdir, walk
+from tempfile import TemporaryDirectory
+from shutil import rmtree
+from argparse import ArgumentParser
+parser = ArgumentParser('md-link-linter')
+parser.add_argument('-g', '--git', action='store_true', help="lint a git repository instead of a directory")
+parser.add_argument('location', nargs='+', help="the directories (or repository urls if --git) to lint")
 args = parser.parse_args()
 links = []
+def read_until(fd, start, stop):
+    fd.seek(start)
+    text = ''
+    char = fd.read(1)
+    while char != '':
+        text += char
+        if char == stop:
+            break
+        char = fd.read(1)
+    return text
 def find_links(fd):
     import re
-    link_pattern = re.compile(r'\[([^!][^]]+)\]\((https?://[^\)]+)\)') # regex for an http link. made a small tweak to fix image finds breaking it.
-    return link_pattern.findall(fd.read()) # returns a list of matches. each match is a tuple of the form: (linktext, linkurl)
-with tempfile.TemporaryDirectory() as clonedir: # create a temporary directory to clone the git repo into
-    os.chdir(clonedir)
-    os.system('git clone -q ' + args.repo + ' .') # clone the repo into the current directory
-    shutil.rmtree('.git') # remove the .git folder so that we don't have to look for markdown files in it
-    for dirname, dirs, files in os.walk('.'): # explores the directory tree
+    text = ''
+    line_positions = [0]
+    new = read_until(fd, 0, '\n')
+    while new != '':
+        text += new
+        line_positions.append(fd.tell())
+        new = read_until(fd, fd.tell(), '\n')
+    for match in re.finditer(r'!?\[([^\[\]]+)\]\((https?:\/\/[^\)]+)\)', text):
+        for position in reversed(line_positions):
+            if position < match.start():
+                break
+        context = read_until(fd, position, '\n')
+        yield match.group(1), match.group(2), context
+    # returns a list of matches. each match is a tuple of the form: (context, linktext, linkurl)
+def check_link(link):
+    return eval(run(['curl', '-s', '-o /dev/null', '-I', '-w "%{http_code}"', link], stdout=PIPE).stdout)
+def find_md_files(directory):
+    for dirname, dirs, files in walk(directory): # explores the directory tree
         for entry in files:
             if entry.endswith('.md'):
-                with open(dirname+'/'+entry, mode='r') as fd: # if it's a markdown file, open it in read mode
+                yield dirname+'/'+entry
+if args.git:
+    for repo in args.location:
+        with TemporaryDirectory() as clonedir: # create a temporary directory to clone the git repo into
+            chdir(clonedir)
+            run(['git','clone','-q',repo,'.'],check=True) # clone the repo into the current directory
+            rmtree('.git') # remove the .git folder so that we don't have to look for markdown files in it
+            for path in find_md_files(clonedir):
+                with open(path, mode='r') as fd: # if it's a markdown file, open it in read mode
                     links += find_links(fd) # search for links
+else:
+    for directory in args.location:
+        chdir(directory)
+        for path in find_md_files(directory):
+            with open(path, mode='r') as fd: # if it's a markdown file, open it in read mode
+                links += find_links(fd) # search for links
 for link in links:
-    print("\n{0[0]}: {0[1]} | Status: [ADDME]".format(link))
+    print("""
+Link found: "{}"
+URL: "{}"
+Status: {}
+
+in this line:
+{}
+""".format(link[0], link[1], check_link(link[1]), link[2]))
+    print('-'*80)
